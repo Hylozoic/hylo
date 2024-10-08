c74568f7803f8b6c4624555703057108a63d8be3
Loren Johnson <lorenjohnson@gmail.com>
2024-10-08 12:06:57 +0200

    Setup @hylo/shared package as ESM only and simplify modules configuration

--- libs/shared/babel.config.js
+++ libs/shared/babel.config.js
@@ -1,11 +1,28 @@
-// Babel config automaticaly configured by microbundle
-// the below is use for jest test runs only
-
-module.exports =
-  process.env.NODE_ENV === 'test'
-    ? {
-        presets: [
-          ['@babel/preset-env']
-        ]
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // Prevents Babel from transpiling ES modules to CommonJS
+        modules: false,
+        targets: {
+          node: 'current'  // Adjust this if necessary based on your Node.js target
+        }
       }
-    : {}
+    ]
+  ],
+  // This section is specifically for running tests with Jest
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            // In test environments, Babel can transpile modules to CommonJS
+            modules: 'commonjs'
+          }
+        ]
+      ]
+    }
+  }
+}
---
+++ libs/shared/index.js
@@ -0,0 +1,12 @@
+import * as TextHelpers from './src/TextHelpers'
+import * as PathHelpers from './src/PathHelpers'
+import * as Validators from './src/Validators'
+import * as LocationHelpers from './src/LocationHelpers'
+export * from './constants'
+
+export default {
+  TextHelpers,
+  PathHelpers,
+  Validators,
+  LocationHelpers
+}
