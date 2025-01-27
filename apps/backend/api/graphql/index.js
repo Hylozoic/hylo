82d0ed6b1f23cbf68187bc0f60e0fccf2b491c12
Loren Johnson <lorenjohnson@gmail.com>
2025-01-27 11:39:43 +0100

    chore(backend): Adds yoga graphql prometheus plugin in dev, and prometheus docker setup for metrics ui at /noo/graphql/metrics

--- apps/backend/api/graphql/index.js
+++ apps/backend/api/graphql/index.js
@@ -1,13 +1,38 @@
 import { createYoga } from 'graphql-yoga'
 import { red } from 'chalk'
 import { inspect } from 'util'
+import { usePrometheus } from '@graphql-yoga/plugin-prometheus'
 import RedisPubSub from '../services/RedisPubSub'
 import makeSchema from './makeSchema'
 
 export const GRAPHQL_ENDPOINT = '/noo/graphql'
 
+const prometheusConfig = {
+  endpoint: `${GRAPHQL_ENDPOINT}/metrics`,
+  // By default, these are the metrics that are enabled:
+  graphql_envelop_request_time_summary: true,
+  graphql_envelop_phase_parse: true,
+  graphql_envelop_phase_validate: true,
+  graphql_envelop_phase_context: true,
+  graphql_envelop_phase_execute: true,
+  graphql_envelop_phase_subscribe: true,
+  graphql_envelop_error_result: true,
+  graphql_envelop_deprecated_field: true,
+  graphql_envelop_request_duration: true,
+  graphql_envelop_schema_change: true,
+  graphql_envelop_request: true,
+  graphql_yoga_http_duration: true,
+  // This metric is disabled by default.
+  // Warning: enabling resolvers level metrics will introduce significant overhead
+  graphql_envelop_execute_resolver: true
+}
+
 export const yoga = createYoga({
   graphqlEndpoint: GRAPHQL_ENDPOINT,
+  plugins: [
+    // See https://the-guild.dev/graphql/yoga-server/docs/features/monitoring
+    process.env.NODE_ENV === 'development' && usePrometheus(prometheusConfig)
+  ],
   schema: makeSchema,
   // plugins: [useLazyLoadedSchema(createSchema)],
   context: async ({ req, params }) => {
