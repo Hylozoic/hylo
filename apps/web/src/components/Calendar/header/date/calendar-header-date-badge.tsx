60d759c43c458fbd64bc4260767525cb8ceaa539
Kevin Triplett <kt@kevintriplett.com>
2025-01-30 17:39:46 -0600

    always report number of events

--- apps/web/src/components/Calendar/header/date/calendar-header-date-badge.tsx
+++ apps/web/src/components/Calendar/header/date/calendar-header-date-badge.tsx
@@ -5,7 +5,6 @@ export default function CalendarHeaderDateBadge() {
   const { events, date } = useCalendarContext()
   const monthEvents = events.filter((event) => isSameMonth(event.start, date))
 
-  if (!monthEvents.length) return null
   return (
     <div className="whitespace-nowrap rounded-sm border px-1.5 py-0.5 text-xs">
       {monthEvents.length} events
