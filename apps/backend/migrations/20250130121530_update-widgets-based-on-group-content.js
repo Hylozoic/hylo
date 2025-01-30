7657bf0a3dbbb6c2d739d32ffdc892ce4d40e17c
Tom <thomasgabriel.watson@gmail.com>
2025-01-30 12:26:50 +1100

    update-widgets-based-on-group-content

---
+++ apps/backend/migrations/20250130121530_update-widgets-based-on-group-content.js
@@ -0,0 +1,24 @@
+
+exports.up = async function(knex) {
+
+  // Find all groups that don't have context widgets
+  const groups = await knex.raw(`
+    SELECT g.id
+    FROM groups g
+  `)
+  const groupIds = groups.rows.map(row => row.id)
+  console.log('Number of updated groups', groupIds.length)
+  // Process each group in its own transaction
+  return Promise.all(groupIds.map(async groupId => {
+    return bookshelf.transaction(async trx => {
+      const group = await Group.find(groupId)
+      if (group) {
+        await group.transitionToNewMenu(trx)
+      }
+    })
+  }))
+};
+
+exports.down = function(knex) {
+  
+};
