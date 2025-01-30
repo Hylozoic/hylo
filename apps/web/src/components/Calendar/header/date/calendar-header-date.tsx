685c0a6d9d9ff003161fc260132b93eeedee6730
Kevin Triplett <kt@kevintriplett.com>
2025-01-30 17:40:14 -0600

    remove redundant date

--- apps/web/src/components/Calendar/header/date/calendar-header-date.tsx
+++ apps/web/src/components/Calendar/header/date/calendar-header-date.tsx
@@ -11,7 +11,6 @@ export default function CalendarHeaderDate() {
       <CalendarHeaderDateIcon />
       <div>
         <div className="flex items-center gap-1">
-          <p className="text-lg font-semibold">{format(date, 'MMMM yyyy')}</p>
           <CalendarHeaderDateBadge />
         </div>
         <CalendarHeaderDateChevrons />
