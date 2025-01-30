62340e5cbcbb7c777572eadfd1f49fda7b773121
Tibet Sprague <tibet.sprague@gmail.com>
2025-01-29 18:28:16 -0800

    Try to fix migrations on staging

--- apps/backend/migrations/20250129142240_add-home-tag-to-all-groups.js
+++ apps/backend/migrations/20250129142240_add-home-tag-to-all-groups.js
@@ -12,52 +12,51 @@ exports.up = async function(knex) {
 
   // console.log(`Adding home tag to ${groups.length} groups`)
 
-  return bookshelf.transaction(async transacting => {
-    // First fetch or create the home tag
-    let homeTag = await Tag.where({ name: 'home' }).fetch({ transacting })
-    if (!homeTag) {
-      homeTag = await Tag.forge({
-        name: 'home',
-        created_at: new Date(),
-        updated_at: new Date()
-      }).save(null, { transacting })
-    }
+  let homeTag = await Tag.where({ name: 'home' }).fetch({ })
+  if (!homeTag) {
+    await Tag.forge({
+      name: 'home',
+      created_at: new Date(),
+      updated_at: new Date()
+    }).save(null, { })
+  }
 
-    // Create GroupTags for each group
-    // XXX: dont run this yet, more thinking to do
-    // await Promise.all(groups.map(async group => {
-    //   // Check if group already has the home tag
-    //   const existingGroupTag = await GroupTag
-    //     .where({
-    //       group_id: group.id,
-    //       tag_id: homeTag.get('id')
-    //     })
-    //     .fetch({ transacting })
-    //     .catch(() => null)
+  // Create GroupTags for each group
+  // XXX: dont run this yet, more thinking to do
+  // await Promise.all(groups.map(async group => {
+  //   // Check if group already has the home tag
+  //   const existingGroupTag = await GroupTag
+  //     .where({
+  //       group_id: group.id,
+  //       tag_id: homeTag.get('id')
+  //     })
+  //     .fetch({ transacting })
+  //     .catch(() => null)
 
-    //   if (!existingGroupTag) {
-    //     await GroupTag.create({
-    //       updated_at: new Date(),
-    //       group_id: group.id,
-    //       tag_id: homeTag.get('id'),
-    //       user_id: group.created_by_id || User.AXOLOTL_ID,
-    //       is_default: true
-    //     }, { transacting })
-    //   }
+  //   if (!existingGroupTag) {
+  //     await GroupTag.create({
+  //       updated_at: new Date(),
+  //       group_id: group.id,
+  //       tag_id: homeTag.get('id'),
+  //       user_id: group.created_by_id || User.AXOLOTL_ID,
+  //       is_default: true
+  //     }, { transacting })
+  //   }
 
-    //   // Update context widgets with title 'widget-hearth' to use home tag id
-    //   await knex('context_widgets')
-    //     .where({
-    //       group_id: group.id,
-    //       title: 'widget-hearth'
-    //     })
-    //     .update({
-    //       view_chat_id: homeTag.get('id'),
-    //       title: null
-    //     })
-    //     .transacting(transacting)
-    // }))
-  })
+  //   // Update context widgets with title 'widget-hearth' to use home tag id
+  //   await knex('context_widgets')
+  //     .where({
+  //       group_id: group.id,
+  //       title: 'widget-hearth'
+  //     })
+  //     .update({
+  //       view_chat_id: homeTag.get('id'),
+  //       title: null
+  //     })
+  //     .transacting(transacting)
+  // }))
+  console.log("add home tag done")
+  return Promise.resolve()
 }
 
 exports.down = async function(knex) {
