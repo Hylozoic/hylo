fe9edb1f8756e0018da596edbfdd44a993a6b0f1
Kevin Triplett <kt@kevintriplett.com>
2025-01-30 20:49:35 -0600

    style calendar day

---
+++ apps/web/src/components/Calendar/body/day/calendar-body-day-event.tsx
@@ -0,0 +1,42 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { postUrl } from 'util/navigation'
+import { useCalendarContext } from 'components/Calendar/calendar-context'
+import { format } from 'date-fns'
+import { CalendarEvent as CalendarEventType } from 'components/Calendar/calendar-types'
+import Tooltip from 'components/Tooltip'
+
+export default function CalendarBodyDayEvent({
+  event
+} : {
+  event: CalendarEventType
+}) {
+  const { routeParams, locationParams, querystringParams } =
+    useCalendarContext()
+  const navigate = useNavigate()
+  const showDetails = useCallback(() => {
+      navigate(postUrl(event.id, routeParams, { ...locationParams, ...querystringParams }))
+  }, [event.id, routeParams, locationParams, querystringParams])
+  // TODO format for multi-day events
+  const toolTipTitle = `${event.title}<br />${format(event.start, 'h:mm a')} - ${format(event.end, 'h:mm a')}`
+
+  return (
+    <div
+      className="flex items-center gap-2 px-2 cursor-pointer"
+      onClick={showDetails}
+      data-tooltip-id={`title-tip-${event.id}`} data-tooltip-html={toolTipTitle}
+    >
+      <div className="flex items-center gap-2">
+        <div className={`size-2 rounded-full bg-${event.color}-500`} />
+        <p className="text-muted-foreground text-sm font-medium">
+          {event.title}
+        </p>
+      </div>
+      <Tooltip
+        delay={550}
+        id={`eventToolTip-${event.id}`}
+        position='right'
+      />
+    </div>
+  )
+}
--- apps/web/src/components/Calendar/body/day/calendar-body-day-events.tsx
+++ apps/web/src/components/Calendar/body/day/calendar-body-day-events.tsx
@@ -1,8 +1,9 @@
 import { useCalendarContext } from '../../calendar-context'
 import { isSameDay } from 'date-fns'
+import CalendarBodyDayEvent from './calendar-body-day-event'
 
 export default function CalendarBodyDayEvents() {
-  const { events, date, setManageEventDialogOpen, setSelectedEvent } =
+  const { events, date } =
     useCalendarContext()
   const dayEvents = events.filter((event) => isSameDay(event.start, date))
 
@@ -11,25 +12,12 @@ export default function CalendarBodyDayEvents() {
       <p className="font-medium p-2 pb-0 font-heading">Events</p>
       <div className="flex flex-col gap-2">
         {dayEvents.map((event) => (
-          <div
-            key={event.id}
-            className="flex items-center gap-2 px-2 cursor-pointer"
-            onClick={() => {
-              setSelectedEvent(event)
-              setManageEventDialogOpen(true)
-            }}
-          >
-            <div className="flex items-center gap-2">
-              <div className={`size-2 rounded-full bg-${event.color}-500`} />
-              <p className="text-muted-foreground text-sm font-medium">
-                {event.title}
-              </p>
-            </div>
-          </div>
+          <CalendarBodyDayEvent key={event.id} event={event} />
         ))}
       </div>
     </div>
   ) : (
     <div className="p-2 text-muted-foreground">No events today...</div>
   )
+
 }
