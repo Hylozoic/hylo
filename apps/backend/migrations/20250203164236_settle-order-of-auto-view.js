23eeebaae226e04aa0b5e4721b0997f8a07f0768
Tibet Sprague <tibet.sprague@gmail.com>
2025-02-03 08:59:03 -0800

    Only settle widget order for active groups

--- apps/backend/migrations/20250203164236_settle-order-of-auto-view.js
+++ apps/backend/migrations/20250203164236_settle-order-of-auto-view.js
@@ -1,13 +1,13 @@
 exports.up = async function(knex) {
   // Fetch all group IDs
-  const groups = await knex('groups').select('id');
+  const groups = await knex('groups').where('active', true).select('id');
   const groupIds = groups.map(group => parseInt(group.id))
 
   // Process each group in its own transaction
   for (const groupId of groupIds) {
     console.log('Settling auto-view order for group', groupId,)
     const trx = await knex.transaction()
-    
+
     const autoViewWidget = await trx('context_widgets')
       .where({
         type: 'auto-view',
@@ -18,7 +18,7 @@ exports.up = async function(knex) {
     try {
       await trx.raw(`
         WITH numbered_widgets AS (
-          SELECT 
+          SELECT
             id,
             ROW_NUMBER() OVER (ORDER BY "order" ASC) as new_order
           FROM context_widgets
@@ -29,7 +29,7 @@ exports.up = async function(knex) {
         FROM numbered_widgets
         WHERE context_widgets.id = numbered_widgets.id
       `, [autoViewWidget.id]);
-      
+
       await trx.commit();
     } catch (error) {
       await trx.rollback();
