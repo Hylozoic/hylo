02e6d1e00c1b6ad1e0493d8f19f977fddfeb7a51
Loren Johnson <lorenjohnson@gmail.com>
2025-02-04 17:20:00 +0100

    chore(mobile): Fix merge issues

--- apps/mobile/src/util/navigation.js
+++ apps/mobile/src/util/navigation.js
@@ -3,7 +3,7 @@ import { host } from 'config'
 import { get, isEmpty, isNumber, omitBy } from 'lodash/fp'
 import qs from 'query-string'
 import { ALL_GROUPS_CONTEXT_SLUG, PUBLIC_CONTEXT_SLUG, MY_CONTEXT_SLUG } from '@hylo/shared'
-import { isContextGroup } from 'presenters/GroupPresenter'
+import { isContextGroup } from '@hylo/presenters/GroupPresenter'
 
 export const HYLO_ID_MATCH = '\\d+'
 export const POST_ID_MATCH = HYLO_ID_MATCH
