8fcca143b77bf900cd65389c034b749e586a8528
Loren Johnson <lorenjohnson@gmail.com>
2025-02-07 09:38:37 +0100

    chore(mobile): Proper application of SafeAreaView

--- apps/mobile/src/components/DrawerMenu/DrawerMenu.js
+++ apps/mobile/src/components/DrawerMenu/DrawerMenu.js
@@ -2,19 +2,21 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import useCurrentGroup from '@hylo/hooks/useCurrentGroup'
 import useRouteParams from 'hooks/useRouteParams'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import ContextMenu from 'components/ContextMenu'
 import ContextSwitchMenu from 'components/ContextSwitchMenu/ContextSwitchMenu'
 
 export default function DrawerMenu (props) {
+  const insets = useSafeAreaInsets()
   const { myHome, groupSlug } = useRouteParams()
+
   useCurrentGroup({ setToGroupSlug: groupSlug })
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
       <ContextSwitchMenu {...props} />
       <ContextMenu />
-    </SafeAreaView>
+    </View>
   )
 }
 
