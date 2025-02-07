2f7566e0f8af89e1b05619b6df7e9bd9b2825024
Loren Johnson <lorenjohnson@gmail.com>
2025-02-07 23:27:34 +0100

    chore(mobile): Handles the reactOn and deleteReaction mutations to no longer force a re-query/invalidate (and break when paginated)

--- apps/mobile/src/hooks/useReactOnEntity.js
+++ apps/mobile/src/hooks/useReactOnEntity.js
@@ -1,14 +1,6 @@
 import { gql, useMutation } from 'urql'
-
-// TODO: URQL - Re-integrate analytics reporting
-// analytics: {
-//   commentId,
-//   eventName: entityType === 'post' ? AnalyticsEvents.POST_REACTION : AnalyticsEvents.COMMENT_REACTION,
-//   emoji: emojiFull,
-//   groupId: groupIds,
-//   postId,
-//   type: entityType === 'post' ? 'post' : 'comment'
-// }
+import { AnalyticsEvents } from '@hylo/shared'
+import mixpanel from 'services/mixpanel'
 
 const REACT_ON_MUTATION = gql` 
   mutation reactOn($entityId: ID, $data: ReactionInput) {
@@ -58,69 +50,39 @@ const DELETE_COMMENT_REACTION_MUTATION = gql`
   mutation deleteCommentReaction($entityId: ID, $data: ReactionInput) {
     deleteReaction(entityId: $entityId, data: $data) {
       id
+      postReactions {
+        id
+        emojiFull
+        user {
+          id
+          name
+          avatarUrl
+        }
+      }
+      myReactions {
+        id
+        emojiFull
+      }
+      peopleReactedTotal
     }
   }
 `
-
-// postReactions {
-//   id
-//   emojiFull
-//   user {
-//     id
-//     name
-//     avatarUrl
-//   }
-// }
-// myReactions {
-//   id
-//   emojiFull
-// }
-// peopleReactedTotal
-// comments {
-//   items {
-//     id
-//     childComments {
-//       items {
-//         id
-//         commentReactionsTotal
-//         commentReactions {
-//           id
-//           emojiFull
-//           user {
-//             id
-//             name
-//             avatarUrl
-//           }
-//         }
-//         myReactions {
-//           id
-//           emojiFull
-//         }
-//       }
-//     }
-//     commentReactionsTotal
-//     commentReactions {
-//       id
-//       emojiFull
-//       user {
-//         id
-//         name
-//         avatarUrl
-//       }
-//     }
-//     myReactions {
-//       id
-//       emojiFull
-//     }
-//   }
-// }
 
 export default function useReactOnEntity () {
   const [, reactOn] = useMutation(REACT_ON_MUTATION)
   const [, deletePostReaction] = useMutation(DELETE_POST_REACTION_MUTATION)
   const [, deleteCommentReaction] = useMutation(DELETE_COMMENT_REACTION_MUTATION)
-  const reactOnEntity = async (entityType, entityId, emojiFull) =>
+  const reactOnEntity = async (entityType, entityId, emojiFull) => {
     reactOn({ entityId, data: { emojiFull, entityType, entityId } })
+    mixpanel.track(
+      entityType === 'post' ? AnalyticsEvents.POST_REACTION : AnalyticsEvents.COMMENT_REACTION, {
+        commentId: entityType === 'comment' && entityId,
+        emoji: emojiFull,
+        postId: entityType === 'post' && entityId,
+        type: entityType === 'post' ? 'post' : 'comment'
+      }
+    )
+  }
 
   const deleteReactionFromEntity = async (entityType, entityId, emojiFull) => entityType === 'post'
     ? deletePostReaction({ entityId, data: { emojiFull, entityType, entityId } })
---
+++ packages/urql/updates/reactions.js
@@ -0,0 +1,83 @@
+import { gql } from 'urql'
+import meQuery from '@hylo/graphql/queries/meQuery'
+
+/*
+
+reactOn and deleteReaction mutation updaters specifically for Comment and ChildComments
+
+The reasons these cache updaters are necessary are due to the return type of reactOn and deleteReaction always being a Post
+which forces the returned selection set to always be a Post resulting in a cache update for the entire Post in the case
+of a Comment or ChildComment reaction.
+
+Without these updaters the returned results from a reactOn or reactionDelete for a Comment results in the entire Post being
+re-queries, in addition to the Stream queries which may contain this Post. This custom updater probably could be avoided
+entirely if we returned a Reaction type from these two methods.
+
+*/
+
+const commentReactionsFragment = gql`
+  fragment _ on Comment {
+    commentReactionsTotal
+    myReactions {
+      id
+      emojiFull
+    }
+    commentReactions {
+      id
+      emojiFull
+      user {
+        id
+        name
+        avatarUrl
+      }
+    }
+  }
+`
+
+export function reactOn (_results, args, cache, info) {
+  const { entityType, entityId, emojiFull } = args.data
+
+  if (entityType !== 'comment') return
+
+  const currentUser = cache.readQuery({ query: meQuery })?.me
+  const commentData = cache.readFragment(commentReactionsFragment, { id: entityId })
+  const myReaction = {
+    __typename: 'Reaction',
+    id: 'new' + Math.random().toString(),
+    emojiFull
+  }
+  const commentReaction = {
+    ...myReaction,
+    user: {
+      __typename: 'Person',
+      id: currentUser.id,
+      name: currentUser.name,
+      avatarUrl: currentUser.avatarUrl
+    }
+  }
+
+  cache.writeFragment(commentReactionsFragment, {
+    id: entityId,
+    commentReactionsTotal: (commentData?.commentReactionsTotal || 0) + 1,
+    myReactions: [...(commentData?.myReactions || []), myReaction],
+    commentReactions: [...(commentData?.commentReactions || []), commentReaction]
+  })
+}
+
+export function deleteReaction (_results, args, cache, info) {
+  const { entityType, entityId, emojiFull } = args.data
+
+  if (entityType !== 'comment') return
+  const currentUser = cache.readQuery({ query: meQuery })?.me
+  const commentData = cache.readFragment(commentReactionsFragment, { id: entityId })
+
+  if (!commentData) return
+  cache.writeFragment(commentReactionsFragment, {
+    id: entityId,
+    commentReactionsTotal: Math.max(0, commentData.commentReactionsTotal - 1),
+    myReactions: commentData.myReactions.filter(r => r.emojiFull !== emojiFull),
+    commentReactions: commentData.commentReactions.filter(
+      r => !(r.emojiFull === emojiFull && r.user.id === currentUser.id)
+    )
+  })
+}
