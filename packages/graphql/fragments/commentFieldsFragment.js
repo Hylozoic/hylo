7060058fe8443ac9bffa13ce30ae00d5bbc79a1f
Loren Johnson <lorenjohnson@gmail.com>
2025-02-07 18:11:17 +0100

    chore(mobile): FlashList'ify Comments SectionList

--- packages/graphql/fragments/commentFieldsFragment.js
+++ packages/graphql/fragments/commentFieldsFragment.js
@@ -8,30 +8,6 @@ export default gql`
       id
       name
       avatarUrl
-      groupRoles {
-        items {
-          id
-          name
-          emoji
-          active
-          groupId
-          responsibilities {
-            items {
-              id
-              title
-              description
-            }
-          }
-        }
-      }
-      membershipCommonRoles {
-        items {
-          id
-          commonRoleId
-          groupId
-          userId
-        }
-      }
     }
     attachments {
       id
