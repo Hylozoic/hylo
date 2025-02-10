00759b1ecdff0faa847979c57ab4f63395f98a4f
Tom <thomasgabriel.watson@gmail.com>
2025-02-10 15:38:09 +1100

    Move GroupWelcomeCheck logic into group presenter; fix group-related issues with GroupWelcomeLanding

--- apps/mobile/src/components/GroupWelcomeCheck/GroupWelcomeCheck.js
+++ apps/mobile/src/components/GroupWelcomeCheck/GroupWelcomeCheck.js
@@ -5,25 +5,15 @@ import useCurrentGroup from '@hylo/hooks/useCurrentGroup'
 
 export default function GroupWelcomeCheck () {
   const navigation = useNavigation()
-  const [{ currentUser }] = useCurrentUser()
   const [{ currentGroup, fetching }] = useCurrentGroup()
-  const currentMembership = currentUser?.memberships &&
-    currentUser.memberships.find(m => m.group.id === currentGroup?.id)
-
-  const { agreementsAcceptedAt, joinQuestionsAnsweredAt, showJoinForm } = currentMembership?.settings || {}
-
-  const numAgreements = currentGroup?.agreements?.total || 0
-
-  const agreementsChanged = (!currentGroup?.isContextGroup && numAgreements > 0) &&
-    (!agreementsAcceptedAt || agreementsAcceptedAt < currentGroup?.settings?.agreementsLastUpdatedAt)
 
   useEffect(() => {
     if (!fetching) {
-      if ((!currentGroup?.isContextGroup && showJoinForm) || agreementsChanged || (currentGroup?.settings?.askJoinQuestions && !joinQuestionsAnsweredAt)) {
+      if ((currentGroup.shouldWelcome)) {
         navigation.navigate('Group Welcome', { groupId: currentGroup?.id })
       }
     }
-  }, [currentGroup?.isContextGroup, fetching, showJoinForm, agreementsChanged, joinQuestionsAnsweredAt])
+  }, [currentGroup, fetching])
 
   return null
 }
