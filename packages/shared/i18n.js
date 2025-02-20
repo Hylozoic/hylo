8bbe643cd8827d89a8e92c3651acbefdab1b2268
Loren Johnson <lorenjohnson@gmail.com>
2025-02-20 01:18:07 +0100

    chore(shared): i18n shared instance with in18n plurals polyfill (will be ignored in Web for browser where it's not needed)

--- packages/shared/i18n.js
+++ packages/shared/i18n.js
@@ -1,13 +1,15 @@
+import 'intl-pluralrules'
 import i18next from 'i18next'
 
-const i18n = i18next.createInstance()
-
-// NOTE: This instance of i18n is currently used for both Web and Mobile, but is not
-// yet applied to Backend. Hence this shared instance is currently only used by the
-// apps and the other @hylo/* front-end only  modules.
+// NOTE: This instance of i18n is currently available for both Web and Mobile and other
+// @hylo/* front-end only  modules, but not yet applied to the Backend.
 // If we want to add functions here in @hylo/shared which which require translation
 // access we need to amend the Backend Sails configuration to have it use this i18n
 // instance. That looks to be do'able but would require a little research. See:
 // https://sailsjs.com/documentation/concepts/internationalization
+
+export const i18n = i18next.createInstance({}, (err, t) => {
+  if (err) return console.log('something went wrong loading', err)
+})
 
 export default i18n
