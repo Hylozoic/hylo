36aa4381264bc0910c6ce753ce94cccd3f7ae4da
Loren Johnson <lorenjohnson@gmail.com>
2025-02-20 14:27:49 +0100

    chore(mobile): Remove routeParams-based setToGroupSlug from DrawerMenu

--- apps/mobile/src/components/DrawerMenu/DrawerMenu.js
+++ apps/mobile/src/components/DrawerMenu/DrawerMenu.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import useCurrentGroup from '@hylo/hooks/useCurrentGroup'
-import useRouteParams from 'hooks/useRouteParams'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import ContextMenu from 'components/ContextMenu'
 import ContextSwitchMenu from 'components/ContextSwitchMenu/ContextSwitchMenu'
 
 export default function DrawerMenu (props) {
   const insets = useSafeAreaInsets()
-  const { groupSlug } = useRouteParams()
-  const [{ fetching }] = useCurrentGroup({ setToGroupSlug: groupSlug })
 
   return (
     <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
       <ContextSwitchMenu {...props} />
-      {!fetching && <ContextMenu />}
+      <ContextMenu />
     </View>
   )
 }
