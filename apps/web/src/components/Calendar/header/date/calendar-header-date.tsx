12e6954894e3a7d602db1be93e2a01730866caa7
Kevin Triplett <kt@kevintriplett.com>
2025-02-25 11:56:46 -0600

    delete calendar "x events"
    This is due to overly complex calculation of number
    of events for week and day modes

--- apps/web/src/components/Calendar/header/date/calendar-header-date.tsx
+++ apps/web/src/components/Calendar/header/date/calendar-header-date.tsx
@@ -1,16 +1,8 @@
 import React from 'react'
 import CalendarHeaderDateChevrons from './calendar-header-date-chevrons'
-import CalendarHeaderDateBadge from './calendar-header-date-badge'
 
 export default function CalendarHeaderDate () {
   return (
-    <div className='flex items-center gap-2'>
-      <div>
-        <div className='flex items-center gap-1'>
-          <CalendarHeaderDateBadge />
-        </div>
-        <CalendarHeaderDateChevrons />
-      </div>
-    </div>
+    <CalendarHeaderDateChevrons />
   )
 }
