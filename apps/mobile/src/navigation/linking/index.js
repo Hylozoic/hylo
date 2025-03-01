c524b86691772b5e0ec1f1dd2bd58a7d9c9a935e
Loren Johnson <lorenjohnson@gmail.com>
2025-03-01 03:22:37 +0100

    chore(mobile): Adds back /h/use-invitation route

--- apps/mobile/src/navigation/linking/index.js
+++ apps/mobile/src/navigation/linking/index.js
@@ -48,8 +48,7 @@ export const routingConfig = {
   // Available in both Auth and Non-Auth state as JoinGroup needs to check for a valid invite and initiate JTW auth
   // but this unlike all other cases, makes JoinGroup responsible for setting returnToOnAuth path when accessed in non-auth context
   '/:context(groups)/:groupSlug/join/:accessCode':                        'JoinGroup',
-  // TODO:  Routing - Test this. When this path is matched when not auth'd, then returnToAuth will be set.
-  '/welcome':                                                             `${AUTH_ROOT_SCREEN_NAME}/Drawer/Tabs/Home Tab/Group Welcome`,
+  '/h/use-invitation':                                                    'JoinGroup',
 
   // Used only for testing HyloEditor loading and config
   '/hylo-editor':                                                          `${AUTH_ROOT_SCREEN_NAME}/HyloEditor`,
