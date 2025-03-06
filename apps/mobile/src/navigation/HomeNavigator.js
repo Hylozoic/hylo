264beaab46fe37bf7540b824155dfd2a449bb420
Loren Johnson <lorenjohnson@gmail.com>
2025-03-07 00:25:15 +0100

    chore(mobile): Handles context switching linking. Adds code comment explanations of linking stages in HomeNavigator

--- apps/mobile/src/navigation/HomeNavigator.js
+++ apps/mobile/src/navigation/HomeNavigator.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import useCurrentGroup from '@hylo/hooks/useCurrentGroup'
+import useCurrentGroup, { useContextGroups } from '@hylo/hooks/useCurrentGroup'
+import useRouteParams from 'hooks/useRouteParams'
+import useChangeToGroup from 'hooks/useChangeToGroup'
 import useOpenInitialURL from 'hooks/useOpenInitialURL'
 import useReturnToOnAuthPath from 'hooks/useReturnToOnAuthPath'
 // Helper Components
@@ -25,8 +27,42 @@ import MapWebView from 'screens/MapWebView/MapWebView'
 const HomeTab = createStackNavigator()
 export default function HomeNavigator ({ navigation }) {
   const [{ currentGroup }] = useCurrentGroup()
+  const changeToGroup = useChangeToGroup()
+  const { myContext, publicContext } = useContextGroups()
+  const { context, groupSlug } = useRouteParams()
+
+  // The following 3 steps are the final steps in linking within the app:
+
+  // === initialURL ===
+  // If the app was launched via navigation to a URL this hook retrieves
+  // that path and navigates to it. The value is cleared is then cleared
+  // from memory but kept here temporarily to use as a flag to disable
+  // initial animations to make navigation to the destination to make it
+  // happen more seamlessly.
   const initialURL = useOpenInitialURL()
+
+  // === returnToOnAuth ===
+  // When the app is launched or brought into focus via navigation to
+  // a path which requires auth, and the user is not authorized, that
+  // path is stored for later use in a zustand global store and then
+  // navigated to here if it existed (as we're always auth'd), and
+  // then cleared from memory.
   useReturnToOnAuthPath()
+
+  // === context and groupSlug ===
+  // The context and groupSlug are retrieved from the screens params
+  // added by any link that has those named params. They are used here
+  // to switch to the home tab to that context/group while the remaining
+  // route params from the matched link are handled in the final
+  // component in the screen path (e.g. id for a post/:id path is retrieved
+  // in the PostDetails component).
+  useEffect(() => {
+    if (context === 'groups' && groupSlug) {
+      changeToGroup(groupSlug, false)
+    } else if ([myContext.slug, publicContext.slug].includes(context)) {
+      changeToGroup(context, false)
+    }
+  }, [context, groupSlug])
 
   const navigatorProps = {
     screenOptions: {
