ebe9151ff5c73348ee78f43beee5b6cd136f882c
Aaron <aaronb@gmail.com>
2025-03-06 15:43:20 -0800

    Made it so you can click on people in search results and open their profile

--- apps/web/src/routes/Search/Search.js
+++ apps/web/src/routes/Search/Search.js
@@ -210,7 +210,10 @@ function PersonCard ({ person, showPerson, highlightProps }) {
   ))
 
   return (
-    <div className='rounded-xl cursor-pointer p-2 flex transition-all bg-card/40 border-2 border-card/30 shadow-md hover:shadow-lg mb-4 relative hover:z-50 hover:scale-105 duration-400'>
+    <div 
+      className='rounded-xl cursor-pointer p-2 flex transition-all bg-card/40 border-2 border-card/30 shadow-md hover:shadow-lg mb-4 relative hover:z-50 hover:scale-105 duration-400 items-center'
+      onClick={() => showPerson(person.id)}
+    >
       <RoundImage url={person.avatarUrl} className={classes.personImage} large />
       <div className='text-foreground'>
         <Highlight {...highlightProps}>
