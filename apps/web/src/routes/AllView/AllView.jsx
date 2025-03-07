0c2edcaa8152a585bccc68826b403a52c03b77da
Tibet Sprague <tibet.sprague@gmail.com>
2025-03-06 20:23:18 -0800

    Cursor pointer on add view button

--- apps/web/src/routes/AllView/AllView.jsx
+++ apps/web/src/routes/AllView/AllView.jsx
@@ -176,7 +176,7 @@ export default function AllViews () {
 
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
-      <div onClick={() => navigate(addQuerystringToPath(location.pathname, { addview: 'yes', cme: 'yes' }))} className='border-2 flex items-center justify-center border-t-foreground/30 border-x-foreground/20 border-b-foreground/10 p-4 background-black/10 rounded-lg border-dashed relative'>
+      <div onClick={() => navigate(addQuerystringToPath(location.pathname, { addview: 'yes', cme: 'yes' }))} className='border-2 flex items-center justify-center border-t-foreground/30 border-x-foreground/20 border-b-foreground/10 p-4 background-black/10 rounded-lg border-dashed relative cursor-pointer'>
         <div className='block text-center'>
           <div>
             <h3 className='text-lg font-semibold  text-foreground m-0'><Icon name='Plus' style={{ fontSize: 30 }} className='text-foreground ml-2' /> {t('Add View')}</h3>
