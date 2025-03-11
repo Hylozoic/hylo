c783dbf3958d53aa4cdc6b2fbb995d2ba8c771a3
Loren Johnson <lorenjohnson@gmail.com>
2025-03-11 09:54:28 +0100

    chore(mobile): Syncs icons with Web, adds "Request" = "Heart" icon exception

--- apps/mobile/src/components/Icon/index.js
+++ apps/mobile/src/components/Icon/index.js
@@ -10,14 +10,17 @@ const glyphMap = config.icons.reduce((m, { properties: { code, name } }) => {
 
 const RawIcon = createIconSet(glyphMap, 'hylo-evo-icons')
 
-function Icon ({ color, ...otherProps }) {
+function Icon ({ name: providedName, color, ...otherProps }) {
+  const name = providedName === 'Request' ? 'Heart' : providedName
+
   if (color && !color.startsWith('#')) {
     color = colors[color]
     if (!color) {
       throw new Error(`unrecognized color name: "${color}"`)
     }
   }
-  return <RawIcon {...{ color, ...otherProps }} />
+
+  return <RawIcon {...otherProps} color={color} name={name} />
 }
 
 export default Icon
