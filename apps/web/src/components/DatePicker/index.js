0779f33f056d72c251fe03e9c26bc6e71c2e1a12
kevintriplett <kt@kevintriplett.com>
2025-03-20 21:57:05 -0700

    fix bug with incorrect function call

--- apps/web/src/components/DatePicker/index.js
+++ apps/web/src/components/DatePicker/index.js
@@ -1,11 +1,11 @@
 import { cn } from 'util/index'
 import classes from './datePicker.module.scss'
 import Datetime from 'react-datetime'
-import { toDateTime } from '@hylo/shared/src/DateTimeHelper'
+import { toDateTime, dateTimeNow } from '@hylo/shared/src/DateTimeHelper'
 import React from 'react'
 
 function isValidDate (current) {
-  const yesterday = toDateTime().minus({ day: 1 })
+  const yesterday = dateTimeNow().minus({ day: 1 })
   return toDateTime(current) > yesterday
 }
 
