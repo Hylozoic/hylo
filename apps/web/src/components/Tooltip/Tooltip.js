479bee034fc624a0f41ecd2dc863c0a23209b64c
Aaron <aaronb@gmail.com>
2025-03-24 11:28:49 -0700

    Removed unused css file

--- apps/web/src/components/Tooltip/Tooltip.js
+++ apps/web/src/components/Tooltip/Tooltip.js
@@ -1,7 +1,6 @@
 import { cn } from 'util/index'
 import React from 'react'
 import { Tooltip as ReactTooltip } from 'react-tooltip'
-import classes from './Tooltip.module.scss'
 
 const Tooltip = (props) => {
   const { id, className, delay, position, place, content } = props
