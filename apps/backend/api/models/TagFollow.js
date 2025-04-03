49211b4634f24ff249454e5664805b1bea98ac34
Tibet Sprague <tibet.sprague@gmail.com>
2025-04-02 20:37:19 -0700

    Display link to change email settings in chat and comment digest emails

--- apps/backend/api/models/TagFollow.js
+++ apps/backend/api/models/TagFollow.js
@@ -198,6 +198,13 @@ module.exports = bookshelf.Model.extend(Object.assign({
       if (!tagFollow.relations.user?.get('email')) continue
 
       const locale = mapLocaleToSendWithUS(tagFollow.relations.user?.get('settings')?.locale || 'en-US')
+
+      const clickthroughParams = '?' + new URLSearchParams({
+        ctt: 'chat_digest_email',
+        cti: tagFollow.relations.user.id,
+        ctcn: tagFollow.relations.group.get('name')
+      }).toString()
+
       const result = await Email.sendChatDigest({
         version: 'Redesign 2025',
         email: tagFollow.relations.user.get('email'),
@@ -206,10 +213,11 @@ module.exports = bookshelf.Model.extend(Object.assign({
           count: postData.length,
           chat_topic: tagFollow.relations.tag.get('name'),
           // For the overall chat room URL use the URL of the last post in the email digest
-          chat_room_url: Frontend.Route.post(posts.models[posts.models.length - 1], tagFollow.relations.group),
+          chat_room_url: Frontend.Route.post(posts.models[posts.models.length - 1], tagFollow.relations.group) + clickthroughParams,
           // date: TextHelpers.formatDatePair(posts[0].get('created_at'), false, false, posts[0].get('timezone')),
+          email_settings_url: Frontend.Route.notificationsSettings(clickthroughParams, tagFollow.relations.user),
           group_name: tagFollow.relations.group.get('name'),
-          group_avatar_url: tagFollow.relations.group.get('avatar_url'),
+          group_avatar_url: tagFollow.relations.group.get('avatar_url') + clickthroughParams,
           posts: postData
         }
       })
