68520099aeb0200073d2cc75937f18a0eefcecac
Aaron <aaronb@gmail.com>
2025-05-09 14:36:03 -0700

    Reintroduced blur

--- apps/web/src/components/PostDialog/PostDialog.jsx
+++ apps/web/src/components/PostDialog/PostDialog.jsx
@@ -41,7 +41,7 @@ const PostDialog = ({
     <Dialog.Root defaultOpen onOpenChange={handleOpenChange}>
       <Dialog.Portal container={container}>
         <Dialog.Overlay
-          className='PostDialog-Overlay bg-black/50 absolute left-0 right-0 bottom-0 grid place-items-center overflow-y-auto z-[100] h-full p-2'
+          className='PostDialog-Overlay bg-black/50 absolute left-0 right-0 bottom-0 grid place-items-center overflow-y-auto z-[100] h-full backdrop-blur-sm p-2'
           style={{ top: `${containerScrollTop}px` }}
         >
           <Dialog.Content onInteractOutside={handleInteractOutside} className='PostDialog-Content min-w-[300px] w-full bg-background p-3 rounded-md z-[41] max-w-[750px] outline-none'>
