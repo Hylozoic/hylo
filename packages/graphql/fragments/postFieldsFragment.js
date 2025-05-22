57fbf0470ab5ec5cff384324ce942d0b098e625a
Tom <thomasgabriel.watson@gmail.com>
2025-05-22 12:10:04 +1000

    Remove unused query fields

--- packages/graphql/fragments/postFieldsFragment.js
+++ packages/graphql/fragments/postFieldsFragment.js
@@ -13,30 +13,6 @@ export const postFieldsFragment = gql`
       id
       name
       avatarUrl
-      groupRoles {
-        items {
-          id
-          name
-          emoji
-          active
-          groupId
-          responsibilities {
-            items {
-              id
-              title
-              description
-            }
-          }
-        }
-      }
-      membershipCommonRoles {
-        items {
-          id
-          commonRoleId
-          groupId
-          userId
-        }
-      }
     }
     createdAt
     clickthrough
