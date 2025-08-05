3857f87354d37527c0c4ff0e7035675b33d81af3
Tom <thomasgabriel.watson@gmail.com>
2025-08-05 15:53:33 +1000

    Built successfully in EAS with a range of additional dependencies from our mobile app

---
+++ apps/mobile-expo/app.config.js
@@ -0,0 +1,82 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "Hylo",
+    slug: "hylo",
+    version: "6.1.6",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.hylo.HyloA"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.hylo.HyloA"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+              [
+          "@sentry/react-native",
+          {
+            organization: process.env.SENTRY_ORG || "hylo",
+            project: "hylo-mobile",
+            hideSourcemaps: false
+          }
+        ],
+      [
+        "onesignal-expo-plugin",
+        {
+          mode: "development"
+        }
+      ],
+      [
+        "@react-native-google-signin/google-signin",
+        {
+          iosUrlScheme: process.env.IOS_GOOGLE_CLIENT_ID
+        }
+      ],
+      "expo-document-picker"
+      // "react-native-vector-icons" - will configure manually later or use @expo/vector-icons instead
+    ],
+    extra: {
+      eas: {
+        projectId: "b09cf94b-bf31-4ff9-8560-e883b0020b0a"
+      },
+      // Environment variables for runtime access
+      oneSignalAppId: process.env.ONESIGNAL_APP_ID,
+      sentryDsn: process.env.SENTRY_DSN_URL,
+      sentryDevDsn: process.env.SENTRY_DEV_DSN_URL,
+      mixpanelToken: process.env.MIXPANEL_TOKEN,
+      intercomAppId: process.env.INTERCOM_APP_ID,
+      intercomAndroidApiKey: process.env.INTERCOM_ANDROID_API_KEY,
+      intercomIosApiKey: process.env.INTERCOM_IOS_API_KEY,
+      apiHost: process.env.API_HOST,
+      hyloWebBaseUrl: process.env.HYLO_WEB_BASE_URL,
+      sessionCookieKey: process.env.SESSION_COOKIE_KEY,
+      mapboxToken: process.env.MAPBOX_TOKEN,
+      iosGoogleClientId: process.env.IOS_GOOGLE_CLIENT_ID,
+      webGoogleClientId: process.env.WEB_GOOGLE_CLIENT_ID
+    },
+    doctor: {
+      reactNativeDirectoryCheck: {
+        listUnknownPackages: false
+      }
+    }
+  }
+};
