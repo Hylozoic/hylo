950a03eb158807437acbf7fd5ac51697fbd4caae
Tibet Sprague <tibet.sprague@gmail.com>
2025-09-11 14:48:36 -0700

    Fix searching members in event invite and attending dialogs
    They now use the radix dialog component
    
    Also improve styling of event invite dialog

--- apps/web/src/components/PostPeopleDialog/PostPeopleDialog.js
+++ apps/web/src/components/PostPeopleDialog/PostPeopleDialog.js
@@ -1,85 +1,81 @@
 import React from 'react'
-import { useTranslation, withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { filter, get } from 'lodash/fp'
-import ModalDialog from 'components/ModalDialog'
+import * as Dialog from '@radix-ui/react-dialog'
 import TextInput from 'components/TextInput'
 import Member from 'components/Member'
 import classes from './PostPeopleDialog.module.scss'
 import { humanResponse } from '@hylo/presenters/EventInvitationPresenter'
 import { bgImageStyle, cn } from 'util/index'
+import { CENTER_COLUMN_ID } from 'util/scrolling'
 
-class PostPeopleDialog extends React.PureComponent {
-  constructor (props) {
-    super(props)
+function PostPeopleDialog ({ members, onClose, currentGroup, title }) {
+  const { t } = useTranslation()
+  const [searchString, setSearchString] = React.useState('')
+  const [selectedMember, setSelectedMember] = React.useState(members[0])
+  const [filteredMembers, setFilteredMembers] = React.useState(members)
 
-    this.state = {
-      searchString: '',
-      selectedMember: this.props.members[0],
-      members: this.props.members
-    }
+  const selectMember = member => () => setSelectedMember(member)
+
+  const search = ({ target }) => {
+    const newSearchString = target.value
+    const membersFilter = m => m.name.toLowerCase().includes(newSearchString.toLowerCase())
+
+    setSearchString(newSearchString)
+    setFilteredMembers(filter(membersFilter, members))
   }
 
-  selectMember = member => () => this.setState({
-    selectedMember: member
-  })
+  const dialogTitle = title ?? t('People')
+  const loading = false
 
-  search = ({ target }) => {
-    const searchString = target.value
-    const membersFilter = (m) => m.name.toLowerCase().includes(searchString.toLowerCase())
-
-    this.setState({
-      searchString,
-      members: filter(membersFilter, this.props.members)
-    })
-  }
-
-  render () {
-    const { members, searchString, selectedMember } = this.state
-    const { onClose, currentGroup, title = this.props.t('People') } = this.props
-    const loading = false
-
-    return (
-      <ModalDialog
-        key='members-dialog'
-        closeModal={onClose}
-        modalTitle={`${title} (${this.props.members.length})`}
-        showCancelButton={false}
-        showSubmitButton={false}
-        style={{ width: '100%', maxWidth: '620px' }}
-      >
-        <div className={classes.container}>
-          {/*
-            Note: Can make memberDetails optional by adding a `withDetails` flag
-            sending in `goToMember` and switchin the onClick on a `MemberRow` to
-            go there instead of showing detail and making adding a conditional
-            style to make width of members-list be 100% in that case.
-          */}
-          <div className={classes.membersList}>
-            {this.props.members.length > 7 && <TextInput
-              className={classes.membersSearchInput}
-              aria-label='members-search'
-              autoFocus
-              label='members-search'
-              name='members-search'
-              onChange={this.search}
-              loading={loading}
-              value={searchString}
-              placeholder={this.props.t('Find a member')}
-                                              />}
-            <section>
-              {members.map(member => <MemberRow
-                member={member}
-                selected={member.id === get('id', selectedMember)}
-                onClick={this.selectMember(member)}
-                key={member.id}
-                                     />)}
-            </section>
-          </div>
-          {selectedMember && <MemberDetail member={selectedMember} currentGroup={currentGroup} />}
-        </div>
-      </ModalDialog>
-    )
-  }
+  return (
+    <Dialog.Root defaultOpen onOpenChange={onClose}>
+      <Dialog.Portal container={document.getElementById(CENTER_COLUMN_ID)}>
+        <Dialog.Overlay
+          className='PostPeopleDialog-Overlay bg-black/50 absolute left-0 right-0 bottom-0 grid place-items-center overflow-y-auto z-[100] h-full backdrop-blur-sm p-2'
+        >
+          <Dialog.Content className='PostPeopleDialog-Content min-w-[300px] w-full bg-background p-3 rounded-md z-[41] max-w-[750px] outline-none relative'>
+            <Dialog.Title className='sr-only'>{dialogTitle}</Dialog.Title>
+            <Dialog.Description className='sr-only'>{dialogTitle}</Dialog.Description>
+            <div className={classes.container}>
+              {/**
+                Note: Can make memberDetails optional by adding a `withDetails` flag
+                sending in `goToMember` and switchin the onClick on a `MemberRow` to
+                go there instead of showing detail and making adding a conditional
+                style to make width of members-list be 100% in that case.
+              */}
+              <div className={classes.membersList}>
+                {members.length > 7 && (
+                  <TextInput
+                    className={classes.membersSearchInput}
+                    aria-label='members-search'
+                    autoFocus
+                    label='members-search'
+                    name='members-search'
+                    onChange={search}
+                    loading={loading}
+                    value={searchString}
+                    placeholder={t('Find a member')}
+                  />
+                )}
+                <section>
+                  {filteredMembers.map(member => (
+                    <MemberRow
+                      member={member}
+                      selected={member.id === get('id', selectedMember)}
+                      onClick={selectMember(member)}
+                      key={member.id}
+                    />
+                  ))}
+                </section>
+              </div>
+              {selectedMember && <MemberDetail member={selectedMember} currentGroup={currentGroup} />}
+            </div>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
 
 function MemberRow ({ member, selected, onClick }) {
@@ -107,4 +103,4 @@ function MemberDetail ({ member, currentGroup }) {
   )
 }
 
-export default withTranslation()(PostPeopleDialog)
+export default PostPeopleDialog
