cd81bd858bd107e1e7a9d225344aea4900ec4a4a
Tibet Sprague <tibet.sprague@gmail.com>
2025-09-12 11:17:26 -0700

    Fix display of post people dialog in dark mode
    For event responses and project members

--- apps/web/src/components/PostPeopleDialog/PostPeopleDialog.js
+++ apps/web/src/components/PostPeopleDialog/PostPeopleDialog.js
@@ -4,7 +4,6 @@ import { filter, get } from 'lodash/fp'
 import * as Dialog from '@radix-ui/react-dialog'
 import TextInput from 'components/TextInput'
 import Member from 'components/Member'
-import classes from './PostPeopleDialog.module.scss'
 import { humanResponse } from '@hylo/presenters/EventInvitationPresenter'
 import { bgImageStyle, cn } from 'util/index'
 import { CENTER_COLUMN_ID } from 'util/scrolling'
@@ -35,19 +34,19 @@ function PostPeopleDialog ({ members, onClose, currentGroup, title }) {
           className='PostPeopleDialog-Overlay bg-black/50 absolute left-0 right-0 bottom-0 grid place-items-center overflow-y-auto z-[100] h-full backdrop-blur-sm p-2'
         >
           <Dialog.Content className='PostPeopleDialog-Content min-w-[300px] w-full bg-background p-3 rounded-md z-[41] max-w-[750px] outline-none relative'>
-            <Dialog.Title className='sr-only'>{dialogTitle}</Dialog.Title>
+            <Dialog.Title>{dialogTitle}</Dialog.Title>
             <Dialog.Description className='sr-only'>{dialogTitle}</Dialog.Description>
-            <div className={classes.container}>
+            <div className='flex flex-wrap h-[380px] content-start'>
               {/**
                 Note: Can make memberDetails optional by adding a `withDetails` flag
                 sending in `goToMember` and switchin the onClick on a `MemberRow` to
                 go there instead of showing detail and making adding a conditional
                 style to make width of members-list be 100% in that case.
               */}
-              <div className={classes.membersList}>
+              <div className='w-1/2 pr-1 flex flex-col h-full'>
                 {members.length > 7 && (
                   <TextInput
-                    className={classes.membersSearchInput}
+                    className='mb-[5px] w-full flex'
                     aria-label='members-search'
                     autoFocus
                     label='members-search'
@@ -58,7 +57,7 @@ function PostPeopleDialog ({ members, onClose, currentGroup, title }) {
                     placeholder={t('Find a member')}
                   />
                 )}
-                <section>
+                <section className='flex-1 overflow-y-auto border border-border rounded'>
                   {filteredMembers.map(member => (
                     <MemberRow
                       member={member}
@@ -83,22 +82,22 @@ function MemberRow ({ member, selected, onClick }) {
   const { name, avatarUrl, response } = member
 
   return (
-    <div className={cn(classes.row, { [classes.selected]: selected })} onClick={onClick}>
-      <div className={classes.col}>
-        <div className={classes.avatar} style={bgImageStyle(avatarUrl)} />
+    <div className={cn('flex items-center w-full cursor-pointer bg-background', selected && 'bg-selected')} onClick={onClick}>
+      <div className='flex items-center justify-start p-[5px]'>
+        <div className='w-10 h-10 rounded-full bg-cover bg-center' style={bgImageStyle(avatarUrl)} />
       </div>
-      <div className={classes.col}>
+      <div className='flex items-center justify-start p-[5px]'>
         {name}
       </div>
-      {response && <div className={cn(classes.col, classes.response)}>{t(humanResponse(response))}</div>}
+      {response && <div className={cn('flex items-center justify-start p-[5px]', 'ml-auto text-[12px]')}>{t(humanResponse(response))}</div>}
     </div>
   )
 }
 
 function MemberDetail ({ member, currentGroup }) {
   return (
-    <div className={classes.memberDetail}>
-      <Member member={member} className={classes.member} group={currentGroup} />
+    <div className='w-1/2'>
+      <Member member={member} className='w-full border-0 p-0 pl-[10px] pb-[10px] m-0 max-w-none' group={currentGroup} />
     </div>
   )
 }
