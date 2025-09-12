39ebc096102d4dbe3a5756ddc87af6ff852dfb20
Tibet Sprague <tibet.sprague@gmail.com>
2025-09-12 13:16:31 -0700

    Fix adding location to chat post on Android
    Also in chat post editor have location results appear above input box so they dont go below the screen

--- apps/web/src/components/LocationInput/LocationInput.js
+++ apps/web/src/components/LocationInput/LocationInput.js
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import { withTranslation } from 'react-i18next'
 import { LocationHelpers } from '@hylo/shared'
 import Geocoder from 'components/GeocoderAutocomplete'
 import { mapbox } from 'config/index'
+import { cn } from 'util/index'
+
 import styles from './LocationInput.module.scss'
-import { withTranslation } from 'react-i18next'
 
 class LocationInput extends Component {
   static propTypes = {
     className: PropTypes.string,
+    inputPosition: PropTypes.string,
     locationObject: PropTypes.object,
     location: PropTypes.string,
     onChange: PropTypes.func,
@@ -20,6 +23,7 @@ class LocationInput extends Component {
   static defaultProps = {
     mapboxToken: mapbox.token,
     className: 'bg-black/20 rounded-lg text-foreground placeholder-foreground/40 w-full p-4',
+    inputPosition: 'top',
     locationObject: null,
     location: '',
     onChange: null,
@@ -54,7 +58,7 @@ class LocationInput extends Component {
   handleSuggest = e => { }
 
   render () {
-    const { id, locationObject, location, placeholder = this.props.t('Search for a location...'), mapboxToken, className } = this.props
+    const { id, inputPosition, locationObject, location, placeholder = this.props.t('Search for a location...'), mapboxToken, className } = this.props
     const centerAt = (locationObject && locationObject.center) || this.state.browserLocation
 
     return (
@@ -71,10 +75,10 @@ class LocationInput extends Component {
           className={className}
           inputPlaceholder={placeholder}
           resultClass={styles.result}
-          resultsClass={styles.suggestions}
+          resultsClass={cn(styles.suggestions, { '!top-auto bottom-[100%]': inputPosition === 'bottom' })}
           resultFocusClass={styles.selectedResult}
           showLoader
-          inputPosition='top'
+          inputPosition={inputPosition}
           proximity={centerAt ? centerAt.lng + ',' + centerAt.lat : ''}
           bbox=''
           types=''
