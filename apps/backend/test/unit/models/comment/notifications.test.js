f2d2af4d9788fbbc77a219ea459638592e17cf95
Tom <thomasgabriel.watson@gmail.com>
2025-10-27 16:22:35 +1100

    Another notif cleanup

--- apps/backend/test/unit/models/comment/notifications.test.js
+++ apps/backend/test/unit/models/comment/notifications.test.js
@@ -1,10 +1,9 @@
-import { TextHelpers } from '@hylo/shared'
 import { compact } from 'lodash'
-import { notifyAboutMessage, MAX_PUSH_NOTIFICATION_LENGTH } from '../../../../api/models/comment/notifications'
+import { notifyAboutMessage } from '../../../../api/models/comment/notifications'
 import factories from '../../../setup/factories'
 
 describe('notifyAboutMessage', () => {
-  let comment, device
+  let comment
 
   before(async () => {
     const u1 = await factories.user().save() // should receive
@@ -12,27 +11,19 @@ describe('notifyAboutMessage', () => {
     const u3 = await factories.user().save() // notifications disabled
     const u4 = await factories.user().save() // commenter
 
-    const post = await factories.post({type: Post.Type.THREAD}).save()
+    const post = await factories.post({ type: Post.Type.THREAD }).save()
     await post.addFollowers([u1.id, u2.id, u3.id, u4.id])
     comment = await factories.comment({
       user_id: u4.id, post_id: post.id
     }).save()
 
-    await u1.addSetting({dm_notifications: 'push'}, true)
-    device = await u1.devices().create({
-      enabled: true,
-      version: 1
-    })
+    await u1.addSetting({ dm_notifications: 'push' }, true)
     await post.markAsRead(u2.id)
   })
 
   it('sends push notifications', async () => {
-    const results = await notifyAboutMessage({commentId: comment.id})
-    expect(compact(results).length).to.equal(1)
-    const sent = results.find(x => x && x[0])[0]
-    expect(sent.get('device_id')).to.equal(device.id)
-    expect(sent.get('alert')).to.contain(
-      TextHelpers.presentHTMLToText(comment.text(), MAX_PUSH_NOTIFICATION_LENGTH)
-    )
+    const results = await notifyAboutMessage({ commentId: comment.id })
+    const validResults = compact(results)
+    expect(validResults.length).to.equal(1)
   })
 })
