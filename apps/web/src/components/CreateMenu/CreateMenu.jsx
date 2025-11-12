6e8e8d1f4a12eceb587b3d7855f8156558e28a8d
Tibet Sprague <tibet.sprague@gmail.com>
2025-11-11 22:56:41 -0800

    Don't show Track and Funding Round in create menu when clicking on the map
    Also tweak styling of create menu

--- apps/web/src/components/CreateMenu/CreateMenu.jsx
+++ apps/web/src/components/CreateMenu/CreateMenu.jsx
@@ -14,7 +14,7 @@ import isWebView from 'util/webView'
 
 const postTypes = Object.keys(POST_TYPES).filter(t => !['action', 'chat', 'submission'].includes(t))
 
-export default function CreateMenu ({ coordinates }) {
+export default function CreateMenu ({ coordinates, mapView }) {
   const routeParams = useRouteParams()
   const location = useLocation()
   const querystringParams = new URLSearchParams(location.search)
@@ -59,7 +59,7 @@ export default function CreateMenu ({ coordinates }) {
             </Link>
           )
         })}
-        {hasTracksResponsibility && (
+        {!mapView && hasTracksResponsibility && (
           <Link to={createTrackUrl(routeParams)} className='text-foreground transition-all hover:scale-105 hover:text-foreground group'>
             <div className='flex text-base items-center p-0 rounded-lg border-2 border-foreground/20 hover:border-foreground/100 transition-all p-1 px-2'>
               <Shapes className='mr-2' />
@@ -68,7 +68,7 @@ export default function CreateMenu ({ coordinates }) {
             </div>
           </Link>
         )}
-        {hasRoundsResponsibility && (
+        {!mapView && hasRoundsResponsibility && (
           <Link to={`${location.pathname}/create/funding-round`} className='text-foreground transition-all hover:scale-105 hover:text-foreground group'>
             <div className='flex text-base items-center p-0 rounded-lg border-2 border-foreground/20 hover:border-foreground/100 transition-all p-1 px-2'>
               <BadgeDollarSign className='mr-2' />
