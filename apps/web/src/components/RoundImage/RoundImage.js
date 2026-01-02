f88f71dd490b89c626ac77db3fc59c3d5c259a58
Aaron <aaronb@gmail.com>
2026-01-02 14:52:22 -0800

    Cleaned up avatar and roundImage styling, updated with tailwind and set up some props that weren't all the way wired

--- apps/web/src/components/RoundImage/RoundImage.js
+++ apps/web/src/components/RoundImage/RoundImage.js
@@ -2,8 +2,10 @@ import { string, bool, func } from 'prop-types'
 import React from 'react'
 import { cn } from 'util/index'
 import { bgImageStyle } from '../../util/index'
-import classes from './RoundImage.module.scss'
 
+/**
+ * RoundImage component displays a circular or square image with various size options
+ */
 export default function RoundImage ({
   url,
   tiny,
@@ -18,29 +20,44 @@ export default function RoundImage ({
   square,
   size,
   onClick,
-  withBorder = true
+  withBorder = false
 }) {
+  // Determine size classes - default is 44px (w-11 h-11)
+  const sizeClasses = xxlarge
+    ? 'w-full h-full max-w-[20vh] max-h-[20vh]'
+    : xlarge
+      ? 'w-20 h-20 min-w-20'
+      : large
+        ? 'w-[40px] h-[40px] min-w-[40px]'
+        : medium
+          ? 'w-8 h-8 min-w-8'
+          : small
+            ? 'w-5 h-5 min-w-5'
+            : tiny
+              ? 'w-3.5 h-3.5'
+              : 'w-11 h-11 min-w-11'
+
   const imageClasses = cn(
-    classes.image,
+    // Base styles
+    'p-1 bg-background transition-all max-w-full inline-block align-top bg-cover bg-center',
+    // Shape - rounded-full by default, rounded-lg if square
+    square ? 'rounded-lg' : 'rounded-full',
+    // Size
+    sizeClasses,
+    // Border
+    withBorder ? 'border-2 border-card' : 'border-0',
+    // Overlaps
     {
-      [classes.square]: square,
-      [classes.overlaps]: overlaps,
-      [classes.tiny]: tiny,
-      [classes.small]: small,
-      [classes.medium]: medium,
-      [classes.large]: large,
-      [classes.xlarge]: xlarge,
-      [classes.xxlarge]: xxlarge,
-      [classes.overlapsVertical]: overlapsVertical
+      '-ml-4 first:-ml-0': overlaps,
+      '-mt-4 first:-mt-0': overlapsVertical
     }
   )
+
   let style = bgImageStyle(url)
   if (size) {
     style = { ...style, width: size, height: size }
   }
-  if (!withBorder) {
-    style = { ...style, borderWidth: 0 }
-  }
+
   return (
     <div
       role='img'
