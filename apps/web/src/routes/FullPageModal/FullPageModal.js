39188822812b996d9057508f0fd935d0fa657a46
Tibet Sprague <tibet.sprague@gmail.com>
2026-03-11 13:51:16 -0700

    Re-add legacy web view handling for group settings

--- apps/web/src/routes/FullPageModal/FullPageModal.js
+++ apps/web/src/routes/FullPageModal/FullPageModal.js
@@ -1,6 +1,7 @@
 import { cn } from 'util/index'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { isLegacyWebView } from 'util/webView'
 
 export default function FullPageModal ({
   confirmMessage, navigate, goToOnClose,
@@ -8,21 +9,39 @@ export default function FullPageModal ({
 }) {
   const multipleTabs = Array.isArray(content)
 
-  return (
-    <div className={cn('bg-midground h-full')}>
-      {multipleTabs && (
-        <div className={cn('w-full max-w-[750px] mx-auto px-2 py-2 sm:px-8 sm:py-8')}>
-          <Routes>
-            {content.map(tab =>
-              <Route
-                path={tab.path}
-                element={tab.render ? tab.render() : tab.component}
-                key={tab.path}
-              />)}
-          </Routes>
-        </div>
-      )}
-      {!multipleTabs && <div className={cn('w-full max-w-[750px] mx-auto px-8 py-8')}>{content || children}</div>}
-    </div>
-  )
+  // DEPRECATED: New mobile app no longer longer renders differently for webview but uses standard layout
+  if (isLegacyWebView()) {
+    return (
+      <div className='bg-background overflow-y-auto relative top-0 p-10'>
+        <Routes>
+          {multipleTabs && content.map(tab => (
+            <Route
+              path={tab.path}
+              element={tab.render ? tab.render() : tab.component}
+              key={tab.path}
+            />
+          ))}
+        </Routes>
+        {!multipleTabs && (content || children)}
+      </div>
+    )
+  } else {
+    return (
+      <div className={cn('bg-midground h-full')}>
+        {multipleTabs && (
+          <div className={cn('w-full max-w-[750px] mx-auto px-2 py-2 sm:px-8 sm:py-8')}>
+            <Routes>
+              {content.map(tab =>
+                <Route
+                  path={tab.path}
+                  element={tab.render ? tab.render() : tab.component}
+                  key={tab.path}
+                />)}
+            </Routes>
+          </div>
+        )}
+        {!multipleTabs && <div className={cn('w-full max-w-[750px] mx-auto px-8 py-8')}>{content || children}</div>}
+      </div>
+    )
+  }
 }
