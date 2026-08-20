d006bbdad577fca282f3f5373d28d06a936cc9d5
Tom <thomasgabriel.watson@gmail.com>
2026-08-20 12:00:50 +1200

    Fix group explorer query selector

--- apps/web/src/hooks/useEnsureSearchedGroups/index.js
+++ apps/web/src/hooks/useEnsureSearchedGroups/index.js
@@ -1,14 +1,15 @@
 import { useEffect } from 'react'
-import { fetchGroups, getGroups, getHasMoreGroups } from 'store/actions/fetchGroups'
+import { fetchGroups, getGroups, getGroupQueryProps, getHasMoreGroups } from 'store/actions/fetchGroups'
 import { useSelector, useDispatch } from 'react-redux'
 import isPendingFor from 'store/selectors/isPendingFor'
 import { FETCH_GROUPS, SORT_NEAREST } from 'store/constants'
 
 export default function useEnsureSearchedGroups ({ sortBy, search, offset, nearCoord, visibility, groupType, farmQuery }) {
   const useNearCoord = sortBy === SORT_NEAREST ? nearCoord : null
-  const groups = useSelector(state => getGroups(state, { sortBy, search, nearCoord: useNearCoord, visibility, groupType, farmQuery, allowedInPublic: true }))
+  const queryProps = getGroupQueryProps({ sortBy, search, nearCoord: useNearCoord, groupType, farmQuery })
+  const groups = useSelector(state => getGroups(state, queryProps))
   const pending = useSelector(state => isPendingFor(FETCH_GROUPS, state))
-  const hasMore = useSelector(state => getHasMoreGroups(state, { sortBy, search, nearCoord: useNearCoord, visibility, groupType, farmQuery, allowedInPublic: true }))
+  const hasMore = useSelector(state => getHasMoreGroups(state, queryProps))
   const dispatch = useDispatch()
 
   useEffect(() => {
